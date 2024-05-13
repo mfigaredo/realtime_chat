045e999ba52313901b014d8fcb9e0746af786fbb
Miguel Figaredo <mfigaredo@gmail.com>
2024-05-12 19:13:33 -0600

    Image send functionality
    Image send functionality

--- a_rtchat/models.py
+++ a_rtchat/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-import shortuuid
+from PIL import Image
+import shortuuid, os
 
 class ChatGroup(models.Model):
     group_name = models.CharField(max_length=128, unique=True, default=shortuuid.uuid)
@@ -20,13 +21,35 @@ class ChatGroup(models.Model):
 class GroupMessage(models.Model):
     group = models.ForeignKey(ChatGroup, related_name='chat_messages', on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.CharField(max_length=300)
+    body = models.CharField(max_length=300, blank=True, null=True)
+    file = models.FileField(upload_to='files/', blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    @property
+    def filename(self):
+        if self.file:
+            return os.path.basename(self.file.name)
+        else:
+            return None
+
     def __str__(self) -> str:
-        return f'{self.author.username} : {self.body[:50]}'
+        if self.body:
+            return f'{self.author.username} : {self.body[:50]}'
+        elif self.file:
+            return f'{self.author.username} : {self.filename}'
     
     class Meta:
         ordering = ['-created']
 
-
+    @property
+    def is_image(self):
+        # if self.filename.lower().endswith(('.jpg', '.jpeg', '.png', '.gif', '.svg', '.webp')):
+        #     return True
+        # else:
+        #     return False
+        try:
+            image = Image.open(self.file)
+            image.verify()
+            return True
+        except:
+            return False
